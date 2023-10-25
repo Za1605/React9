9d178c57b31922c4339a87a9b676467ead2c55a0
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-10-25 19:31:19 +0300

    hw-3 second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const App = () => {
+    const save = (e) => {
+      e.preventDefault()
+
+    }
+
+
     return (
         <div>
-          
+          <form>
+              <input type={'text'} placeholder={'name'} name={'name'}/>
+              <input type={'text'} placeholder={'age'} name={'age'}/>
+              <button> save</button>
+          </form>
         </div>
     );
 };
