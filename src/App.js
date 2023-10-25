4878647864c2bccb66c762700beb5f9c441e994c
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-10-25 19:31:19 +0300

    hw-3 third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const App = () => {
+    const name = useRef();
+    const age = useRef();
     const save = (e) => {
       e.preventDefault()
+        //console.log(e.form.name.value);
+      //console.log(e.form.age.value);
+        console.log(name.current.value);
+        console.log(age.current.value);
 
     }
 
 
     return (
         <div>
-          <form>
-              <input type={'text'} placeholder={'name'} name={'name'}/>
-              <input type={'text'} placeholder={'age'} name={'age'}/>
-              <button> save</button>
+          <form onSubmit={save}>
+              <input type={'text'} placeholder={'name'} ref={'name'}/>
+              <input type={'text'} placeholder={'age'} ref={'age'}/>
+              <button>save</button>
           </form>
         </div>
     );
