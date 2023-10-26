c070ed2e2ad38ea6c81ea2dc6d9dafee921caa06
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-10-26 22:47:32 +0300

    hw-3 four commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 
 const App = () => {
+    const [user, setUser] = useState();
     const name = useRef();
     const age = useRef();
     const save = (e) => {
       e.preventDefault()
+        e.target.reset();
         //console.log(e.form.name.value);
       //console.log(e.form.age.value);
         console.log(name.current.value);
@@ -12,12 +14,17 @@ const App = () => {
 
     }
 
+    const hendler = (e) => {
+      e.preventDefault()
+        setUser(yuliiaT => ({...yuliiaT, [e.target.name] : e.target.value}))
+    }
+
 
     return (
         <div>
           <form onSubmit={save}>
-              <input type={'text'} placeholder={'name'} ref={'name'}/>
-              <input type={'text'} placeholder={'age'} ref={'age'}/>
+              <input type={'text'} placeholder={'name'} name={'name'}/>
+              <input type={'text'} placeholder={'age'} name={'age'}/>
               <button>save</button>
           </form>
         </div>
