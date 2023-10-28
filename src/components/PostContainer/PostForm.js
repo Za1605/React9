420115b47e253446667745824023281ceeff4e11
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-10-29 00:13:59 +0300

    third

--- src/components/PostContainer/PostForm.js
+++ src/components/PostContainer/PostForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+import {postServise} from "../../servises/postServise";
 
 const PostForm = () => {
     const {reset,handleSubmit, register} = useForm();
-    const save = (post) =>{
-        console.log(post);
+    const save = async (post) =>{
+        const {data} = await postServise.create(post);
+    console.log(data);
     }
     return (
         <form onSubmit={handleSubmit(save)}>
