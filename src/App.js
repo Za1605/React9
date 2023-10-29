2f2d67b98339e426978075c9fdb56b6e40691cd0
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-10-29 19:33:53 +0200

    3 лекція дз останнє

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {UsersContainer} from "./components/UsersContainer/UsersContainer";
+import CommentsContainer from "./components/CommentsContainer/CommentsContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div style={{display: 'flex'}}>
+            <UsersContainer/>
+            <CommentsContainer/>
+        </div>
+    );
+};
 
 export default App;
